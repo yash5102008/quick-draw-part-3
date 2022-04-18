45053991fb710635051a02f196abd7e330d2e59b
yash5102008 <75563395+yash5102008@users.noreply.github.com>
2022-04-18 08:23:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function preload() {
+    classifier = ml5.imageClassifier('doodleNet');
+}
+
+function setup()
+{
+    canvas= createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function clearCanvas() {
+    background("white");
+}
+
+function draw() {
+}
+    strokeWeight(15);
+    stroke(0);
+
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    console.log(results);document.getElementById('label').innerHTML = 'label'  + results[0].label;
+
+    document.getElementById('confidence').innerHTML = 'confidence'  + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new speechSynthesisisUtterrance(results[0].label);
+    synth.speak(utterThis);
+}
+
+
+
